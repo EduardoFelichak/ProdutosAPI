4f5fe90c56e3712b0d4f6a2892ff726eab691ceb
EduardoFelichak <edukf90@gmail.com>
2025-06-09 21:20:29 -0300

    fix: ajuste cors

--- Program.cs
+++ Program.cs
@@ -17,6 +17,17 @@ namespace ProdutosAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            var myAllowSpecificOrigins = "_myAllowSpecificOrigins";
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(name: myAllowSpecificOrigins,
+                    policy =>
+                    {
+                        policy.AllowAnyOrigin()
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
+                    });
+            });
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -82,7 +93,7 @@ namespace ProdutosAPI
             }
 
             app.UseHttpsRedirection();
-
+            app.UseCors(myAllowSpecificOrigins);
             app.UseAuthorization();
 
 
